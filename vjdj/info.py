a7fb5cfea7ee61aabbc70b07dcdb2fbe986ce605
vijay anand <98573917+Vijay-0404@users.noreply.github.com>
2022-03-10 13:05:11 +0530

    Update info.py

--- vjdj/info.py
+++ vjdj/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vijayananddjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'Django@2002'
+EMAIL_HOST_USER = 'enter your email'
+EMAIL_HOST_PASSWORD = 'enter your password'
 EMAIL_PORT = 587
